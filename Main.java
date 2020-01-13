7419e5b7c42ca56c95e0c1fcc6045b788b962cf3
Yoav Levi <yoav.levi@koala.website>
2020-01-13 02:07:14 +0000

    added stuff

--- Main.java
+++ Main.java
@@ -1,5 +1,38 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+class Main
+{
+  public static void main(String[] args)
+  {
+    Student obj = new Student();
+    obj.getResults();
   }
+}
+
+interface IStudent 
+{
+  int getResults();
+  int getsStatus();
+}
+
+class Student implements IStudent 
+{
+  public int getResults()
+  {
+    System.out.println("getResults");
+    return 0;
+  }
+
+  public String getsStatus()
+  {
+    System.out.println("getsStatus");
+    return "hello";
+  }
+
+  String ID, course;
+  int result;
+  String status;
+}
+
+abstract class Person
+{
+  String name,surname,age,gender;
 }
